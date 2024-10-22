c757d2ca6999e0285f89975516debe43a44f6391
Christian Fischerauer <christian.fischerauer@gmail.com>
2024-10-22 13:24:26 +0200

    feat(vscode): first shot of implementation

--- src/XamlStyler.Extension.VSCode/src/extension.ts
+++ src/XamlStyler.Extension.VSCode/src/extension.ts
@@ -1,25 +1,75 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+import * as os from "os";
+import { randomBytes } from "crypto";
+import { exec } from "child_process";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.languages.registerDocumentFormattingEditProvider(
+    "AXAML",
+    {
+      provideDocumentFormattingEdits(document): Promise<vscode.TextEdit[]> {
+        const firstLine = document.lineAt(0);
+        const lastLine = document.lineAt(document.lineCount - 1);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "xamlstyler" is now active!');
+        let text = document.getText();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('xamlstyler.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from XAML Styler!');
-	});
+        // store the text into a temporary file of the vscode extension in order to process it with an external tool in the next step
+        const tempDir = os.tmpdir();
+        const filename = path.join(
+          tempDir,
+          "vscode",
+          "xamlstyler",
+          randomBytes(16).toString("hex")
+        );
 
-	context.subscriptions.push(disposable);
+        fs.mkdirSync(path.dirname(filename), { recursive: true });
+
+        fs.writeFileSync(filename, text);
+
+        // process the text with an external tool
+        return runXStyler(filename).then((newText) => {
+          // remove the temporary file
+          fs.unlinkSync(filename);
+
+          return [
+            vscode.TextEdit.replace(
+              new vscode.Range(firstLine.range.start, lastLine.range.end),
+              newText
+            ),
+          ];
+        });
+      },
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+function runXStyler(filePath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    // Construct the xstyler command with necessary parameters
+    const command = `xstyler --file "${filePath}" --write-to-stdout --ignore`;
+
+    // Run the command using child_process
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error} ${stderr}`);
+        return;
+      }
+      // if (stderr) {
+      //   console.info(`xstyler: ${stderr}`);
+      // }
+      resolve(stdout);
+    });
+  });
 }
 
 // This method is called when your extension is deactivated
